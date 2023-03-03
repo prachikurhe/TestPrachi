4099e2670105094ab5589f7a8671c603143acc23
prachi.kurhe <kurheprachi84@gmail.com>
2023-03-03 22:41:22 +0530

    first commit

---
+++ src/main/java/com/yash/ems/TestPrachi/TestPrachiApplication.java
@@ -0,0 +1,13 @@
+package com.yash.ems.TestPrachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPrachiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPrachiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/ems/TestPrachi/TestPrachiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.ems.TestPrachi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPrachiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
